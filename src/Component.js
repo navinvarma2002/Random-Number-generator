f6b2899f6370f2be968f78850e7b472c14614c25
navinvarma2002 <navinvarma.2002@gmail.com>
2024-06-10 01:53:11 -0700

    updated

---
+++ src/Component.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import "./style.css"
+
+
+
+function Counter(){
+
+
+    var [count,SetCount] = useState(0)
+
+    function Random(){
+
+        SetCount(Math.floor(Math.random()*50)+1)
+
+    }
+
+  
+
+return(<div className="First">
+
+     <h1 className="number">{count}</h1>
+    <button onClick={Random} className="Second">Random Number</button>
+    
+
+
+    </div>)
+
+
+}
+
+export default Counter
